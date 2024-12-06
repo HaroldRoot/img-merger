addcfb8ef1e35d71d45318e79f23747bcb9d5a7f
HaroldRoot <ngoksausi618@gmail.com>
2024-12-06 10:43:51 +0800

    add custom logger to img2ascii

--- img2ascii.py
+++ img2ascii.py
@@ -2,30 +2,32 @@ import argparse
 
 from PIL import Image, UnidentifiedImageError
 
+from custom_logger import logger
+
 
 def generate_ascii_from_image(image_path, max_width=160, max_height=90,
                               output_file=None):
     try:
         with Image.open(image_path) as img:
             original_width, original_height = img.size
-            print(
-                f"Successfully loaded image! "
-                f"Original image size: {original_width} x {original_height}")
+            logger.info(f"Successfully loaded image! "
+                        f"Original image size: "
+                        f"{original_width} x {original_height}")
 
             img.thumbnail((max_width, max_height))
             width, height = img.size
-            print(f"Resized image size: {width} x {height}")
+            logger.info(f"Resized image size: {width} x {height}")
 
             pixel_data = [[img.getpixel((x, y)) for x in range(width)] for y in
                           range(height)]
-            print(f"Successfully constructed pixel data! "
-                  f"Pixel data size: {width} x {height}")
+            logger.info(f"Successfully constructed pixel data! "
+                        f"Pixel data size: {width} x {height}")
 
             luminosity_values = [
                 [int(0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2]) for
                  pixel in row] for row in pixel_data]
-            print(f"Successfully constructed luminosity values! "
-                  f"Luminosity values size: {width} x {height}")
+            logger.info(f"Successfully constructed luminosity values! "
+                        f"Luminosity values size: {width} x {height}")
 
             characters = ("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvcz"
                           "XYUJCLQ0OZmwqpdbkhao*#MW&8%B@$")
@@ -38,8 +40,8 @@ def generate_ascii_from_image(image_path, max_width=160, max_height=90,
                     ascii_row.append(characters[index])
                 ascii_art.append(ascii_row)
 
-            print(f"Successfully constructed ASCII art! "
-                  f"ASCII art size: {width} x {height}")
+            logger.info(f"Successfully constructed ASCII art! "
+                        f"ASCII art size: {width} x {height}")
 
             ascii_output = "\n".join(
                 ["".join([char * 2 for char in row]) for row in ascii_art])
@@ -47,16 +49,17 @@ def generate_ascii_from_image(image_path, max_width=160, max_height=90,
             if output_file:
                 with open(output_file, "w") as f:
                     f.write(ascii_output)
-                print(f"ASCII art has been saved to {output_file}")
+                logger.info(f"ASCII art has been saved to {output_file}")
             else:
                 print(ascii_output)
 
     except FileNotFoundError:
-        print(f"Error: The specified image file '{image_path}' was not found.")
+        logger.error(f"Error: The specified image file '{image_path}' was not "
+                     f"found.")
     except UnidentifiedImageError:
-        print(f"Error: The file '{image_path}' is not a valid image.")
+        logger.error(f"Error: The file '{image_path}' is not a valid image.")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logger.error(f"An unexpected error occurred: {e}")
 
 
 def parse_args():
@@ -75,13 +78,12 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    print(f"Using the following parameters:")
-    print(f"Max width: {args.width}")
-    print(f"Max height: {args.height}")
+    logger.info(f"Using the following parameters:")
+    logger.info(f"Max width: {args.width}")
+    logger.info(f"Max height: {args.height}")
 
     if args.output:
-        print(f"Output will be saved to: {args.output}")
+        logger.info(f"Output will be saved to: {args.output}")
 
     for path in args.input_paths:
-        generate_ascii_from_image(path, args.width, args.height,
-                                  args.output)
+        generate_ascii_from_image(path, args.width, args.height, args.output)
